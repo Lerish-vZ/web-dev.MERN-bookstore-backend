b5cdccfc14af156de169266e0732968010914ffd
Lerish-vZ <lerishavz@gmail.com>
2024-10-24 11:58:07 +0200

    update

--- index.js
+++ index.js
@@ -95,6 +95,14 @@ app.put('/books/:id', async (request, response) => {
 //Route to delete a book
 app.delete('/books/:id', async (request, response) => {
   try {
+    const { id } = request.params;
+    const result = await Book.findByIdAndDelete(id);
+
+    if (!result) {
+      return response.status(404).json({ message: 'Book not found' });
+    }
+    
+    return response.status(200).send({ message: 'Book deleted successfully' });
     
   } catch (error) {
     console.log(error.message);
