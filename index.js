0a6a07efbd4a06c1d93eeb3e49c2f506a0d279bd
Lerish-vZ <lerishavz@gmail.com>
2024-10-24 10:20:21 +0200

    update

--- index.js
+++ index.js
@@ -13,11 +13,10 @@ mongoose
   .connect(mongoDBURL)
   .then(() => {
     console.log(`App connected to database.`);
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
   })
   .catch((error) => {
     console.log(error);
-  });
-
-  app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`);
   });
