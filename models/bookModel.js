5980af4ed076f93d61bebd1fecad0c11589b714d
Lerish-vZ <lerishavz@gmail.com>
2024-10-24 11:13:40 +0200

    update

--- models/bookModel.js
+++ models/bookModel.js
@@ -1,4 +1,11 @@
 import mongoose from "mongoose";
 
-
+const bookSchema = mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        }
+    }
+)
 export const Book = mongoose.model('Book')
