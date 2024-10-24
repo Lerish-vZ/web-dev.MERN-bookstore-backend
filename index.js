f6053423bb3bdddd3d01c36d8a68bf38eefce4d9
Lerish-vZ <lerishavz@gmail.com>
2024-10-24 10:19:34 +0200

    update

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const app = express();
 
-app.get('/', (request, response) => {
-    console.log(request);
-    return response.status(234).send('Welcome to MERN Stack Tutorial')
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Welcome to MERN Stack Tutorial");
 });
 
 app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`)
+  console.log(`App is listening to port: ${PORT}`);
 });
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log(`App connected to database.`);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
