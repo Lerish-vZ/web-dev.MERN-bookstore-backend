7ab5a688cb917be4654e54985d86bc0db189f8da
Lerish-vZ <lerishavz@gmail.com>
2024-11-15 09:54:39 +0200

    update

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 export const PORT = 5555;
-export const mongoDBURL = 'mongodb+srv://lerishavz:lerishavz@book-store-mern.rf5cy.mongodb.net/books?retryWrites=true&w=majority&appName=Book-Store-MERN';
+export const mongoDBURL = 'mongodb+srv://lerishavz:lerishavz@book-store-mern.rf5cy.mongodb.net/?retryWrites=true&w=majority&appName=Book-Store-MERN';
